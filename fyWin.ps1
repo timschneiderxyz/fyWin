4058957352199bd5217751006fb79b7b4efb0f21
Tim Schneider <timschneider.xyz@gmail.com>
2022-07-30 11:38:38 +0200

    renamed project

--- fyWin.ps1
+++ fyWin.ps1
@@ -1,14 +1,14 @@
 # Requires -RunAsAdministrator
 Write-Host -ForegroundColor Blue @"
 
-           .d888          888       888 d8b           d888   .d8888b.
-          d88P"           888   o   888 Y8P          d8888  d88P  Y88b
-          888             888  d8b  888                888  888    888
-          888888 888  888 888 d888b 888 888 88888b.    888  888    888
-          888    888  888 888d88888b888 888 888 "88b   888  888    888
-          888    888  888 88888P Y88888 888 888  888   888  888    888
-          888    Y88b 888 8888P   Y8888 888 888  888   888  Y88b  d88P
-          888     "Y88888 888P     Y888 888 888  888 8888888 "Y8888P"
+           .d888          888       888 d8b
+          d88P"           888   o   888 Y8P
+          888             888  d8b  888
+          888888 888  888 888 d888b 888 888 88888b.
+          888    888  888 888d88888b888 888 888 "88b
+          888    888  888 88888P Y88888 888 888  888
+          888    Y88b 888 8888P   Y8888 888 888  888
+          888     "Y88888 888P     Y888 888 888  888
                       888
                  Y8b d88P
                   "Y88P"
@@ -20,7 +20,7 @@ Write-Host -ForegroundColor Red @"
 
           Author     : Tim Schneider
           Website    : https://timschneider.xyz
-          Repository : https://github.com/RanzigeButter/fyWin10
+          Repository : https://github.com/timschneiderxyz/fyWin
 
           ------------------------------------------------------------
 
